9c5b779c25012ab9fb34b704bcc63c6e054095b9
uli <1252670924@qq.com>
2023-05-18 20:21:19 +0800

    项目初始化完成

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 // 导入全局样式
 import './styles/main.less'
 //CSS 重置
@@ -14,5 +16,5 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+app.use(ElementPlus)
 app.mount('#app')
