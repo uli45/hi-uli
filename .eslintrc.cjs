c6ff6ac430232d311e36b637d4cbac0eb9006609
uli <1252670924@qq.com>
2023-09-17 15:12:51 +0800

    [feat]增加禁用调试包测试中...

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,36 +1,31 @@
-/* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
-
 module.exports = {
-  root: true,
-  extends: [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest'
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
   },
-  rules: {
-    'prettier/prettier': [
-      'warn',
-      {
-        singleQuote: true,
-        semi: false,
-        printWidth: 100,
-        trailingComma: 'none',
-        endOfLine: 'auto'
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:vue/vue3-essential'
+  ],
+  overrides: [
+    {
+      env: {
+        node: true
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script'
       }
-    ],
-    'vue/multi-word-component-names': [
-      'warn',
-      {
-        ignores: ['index']
-      }
-    ],
-    'vue/no-setup-props-destructure': ['off']
-    // 保存代码时缩进4个空格
-    // indent: ['error', 4]
-  }
+    }
+  ],
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module'
+  },
+  plugins: ['@typescript-eslint', 'vue'],
+  rules: {}
 }
--- src/main.ts
+++ src/main.ts
@@ -20,6 +20,17 @@ import '@/assets/styles/common.less'
 //自定义全局指令
 import directive from '@/utils/index'
 
+// 禁用调试包
+import DisableDevtool from 'disable-devtool'
+
+const options = {
+  disableIframeParents: true,
+  disableDevtoolAuto: true,
+  md5: 'dd55cec2ce59aca4e6647dcfbc90dc27'
+}
+
+DisableDevtool(options)
+
 const app = createApp(App)
 
 app.config.globalProperties.$DisableRightClick = true
@@ -40,32 +51,32 @@ app.use(ElementPlus, { size: 'small' })
 app.use(plugins)
 app.mount('#app')
 
-if (import.meta.env.PROD) {
-  // 在生产环境下执行的代码
-  document.addEventListener('contextmenu', (e) => {
-    if (app.config.globalProperties.$DisableRightClick) {
-      e.preventDefault()
-      window.alert('本站已禁止右键!')
-      app.config.globalProperties.$message.error('本站已禁止右键!')
-    }
-  })
-  document.addEventListener('keydown', (e) => {
-    if (app.config.globalProperties.$DisableF12 && e.key === 'F12') {
-      e.preventDefault()
-      window.alert('本站已禁止右键!')
-      app.config.globalProperties.$message.error('本站已禁止F12!')
-    }
-  })
+// if (import.meta.env.PROD) {
+//   // 在生产环境下执行的代码
+//   document.addEventListener('contextmenu', (e) => {
+//     if (app.config.globalProperties.$DisableRightClick) {
+//       e.preventDefault()
+//       window.alert('本站已禁止右键!')
+//       app.config.globalProperties.$message.error('本站已禁止右键!')
+//     }
+//   })
+//   document.addEventListener('keydown', (e) => {
+//     if (app.config.globalProperties.$DisableF12 && e.key === 'F12') {
+//       e.preventDefault()
+//       window.alert('本站已禁止右键!')
+//       app.config.globalProperties.$message.error('本站已禁止F12!')
+//     }
+//   })
 
-  if (app.config.globalProperties.$DisableDevTools) {
-    const element = document.createElement('script')
-    element.textContent = `
-    (function(window, document) {
-      window.alert = function(message) {
-        console.log(message);
-      };
-    })(window, document);
-  `
-    document.head.append(element)
-  }
-}
+//   if (app.config.globalProperties.$DisableDevTools) {
+//     const element = document.createElement('script')
+//     element.textContent = `
+//     (function(window, document) {
+//       window.alert = function(message) {
+//         console.log(message);
+//       };
+//     })(window, document);
+//   `
+//     document.head.append(element)
+//   }
+// }
